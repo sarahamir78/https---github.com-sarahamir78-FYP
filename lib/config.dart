7b4b90134d6a83695f23f0ecfec8700ccd9094b2
faizanabid176 <faizanabid4000@gmail.com>
2024-06-09 22:19:55 +0500

    Latest changes about link

--- lib/config.dart
+++ lib/config.dart
@@ -4,5 +4,5 @@ var schemecolor = Color.fromRGBO(0, 150, 136, 1);
 var bgcolor = Color(0xfff6f6f6);
 var textboxcolor = Color.fromARGB(255, 255, 255, 255);
 var primaycolor = const Color.fromARGB(125, 0, 255, 8);
-var configrul = 'https://comvision.pk/Mart7Grocers/';
+var change = const Color.fromARGB(125, 0, 255, 8);
 var loginicon = const Color(0xffcccccc);
